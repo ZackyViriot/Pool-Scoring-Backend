077d89c928f74396544362aa6e0479269bc30265
Zacky Viriot <zackyviriot987@gmail.com>
2024-12-28 23:04:59 -0600

    changes

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://pool-scoring-frontend.vercel.app',
+      'https://pool-scoring.vercel.app'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap(); 
