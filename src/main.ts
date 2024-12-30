d175de581135314fa5dabf85cd2ff443055b5b5a
Zacky Viriot <zackyviriot987@gmail.com>
2024-12-30 00:27:59 -0600

    updated cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   // Enable CORS for the specific frontend domain
   app.enableCors({
     origin: [
-      'http://pool-scoring-frontend.vercel.app',
+      'https://pool-scoring-frontend.vercel.app',
       'http://localhost:3000'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
