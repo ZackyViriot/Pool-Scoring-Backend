c3875fcd7f042d454a6aeacb2e2ee9aa639372f3
zackyviriot <zackyviriot987@gmail.com>
2026-02-27 11:51:27 -0600

    changes

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -16,7 +16,7 @@ export class PaymentService {
     }
     
     this.stripe = new Stripe(stripeKey, {
-      apiVersion: '2024-12-18.acacia',
+      apiVersion: '2025-02-24.acacia',
     });
   }
 
